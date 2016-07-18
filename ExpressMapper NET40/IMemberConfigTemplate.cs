8ff080a396f5c9f47bf86d818ff35b5a0393a1a6
ikopcanski <ivan.kopcanski@hotmail.com>
2016-07-18 06:51:55 +0400

    BaseOn() - draft version of support for mappings based on previous base types mappings

---
+++ ExpressMapper NET40/IMemberConfigTemplate.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpressMapper
+{
+    public interface IMemberConfigTemplate<TBase, TNBase> : IMemberConfiguration<TBase, TNBase>
+    {
+        void Configure<T, TN>(IMemberConfiguration<T, TN> configuration) where T : TBase where TN : TNBase;
+    }
+}
---
+++ ExpressMapper NET40/MemberConfigTemplate.cs
@@ -0,0 +1,69 @@
+﻿
+using System;
+using System.Linq.Expressions;
+
+namespace ExpressMapper
+{
+    public class MemberConfigTemplate<TBase, TNBase> : IMemberConfigTemplate<TBase, TNBase>
+    {
+        public void Configure<T, TN>(IMemberConfiguration<T, TN> configuration) where T : TBase where TN : TNBase
+        {
+
+        }
+
+        public IMemberConfiguration<TBase, TNBase> InstantiateFunc(Func<TBase, TNBase> constructor)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> Instantiate(Expression<Func<TBase, TNBase>> constructor)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> Before(Action<TBase, TNBase> beforeHandler)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> After(Action<TBase, TNBase> afterHandler)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> Member<TBaseMember, TNBaseMember>(Expression<Func<TNBase, TNBaseMember>> dest, Expression<Func<TBase, TBaseMember>> src)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> Function<TMember, TNMember>(Expression<Func<TNBase, TNMember>> dest, Func<TBase, TMember> src)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> Value<TNMember>(Expression<Func<TNBase, TNMember>> dest, TNMember value)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> Ignore<TMember>(Expression<Func<TNBase, TMember>> dest)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> CaseSensitive(bool caseSensitive)
+        {
+            return this;
+        }
+
+        public IMemberConfiguration<TBase, TNBase> CompileTo(CompilationTypes compilationType)
+        {
+            return this;
+        }
+        
+        public IMemberConfiguration<TBase, TNBase> Flatten()
+        {
+            return this;
+        }
+    }
+}
